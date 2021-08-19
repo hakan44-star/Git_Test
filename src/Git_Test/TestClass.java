db61c5c2e8d640a3d3d07b34292d12fdbc79c7a7
hakan44-star <77295878+hakan44-star@users.noreply.github.com>
2021-08-19 16:30:37 +0200

    Update TestClass.java

--- src/Git_Test/TestClass.java
+++ src/Git_Test/TestClass.java
@@ -3,6 +3,8 @@ package Git_Test;
 public class TestClass {
     public static void main(String[] args) {
         System.out.println("Git den merhaba");
+        System.out.println("dünyadan den merhaba");
+      
     }
 
 
